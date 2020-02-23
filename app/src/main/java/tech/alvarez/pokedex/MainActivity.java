d1d460a2bd61ff65d67ef5f9e7970e212190450b
Daniel Alvarez <daniel@alvarez.tech>
2020-02-23 11:14:09 -0400

    AndroidX migration, gradle update

--- app/src/main/java/tech/alvarez/pokedex/MainActivity.java
+++ app/src/main/java/tech/alvarez/pokedex/MainActivity.java
@@ -1,10 +1,11 @@
 package tech.alvarez.pokedex;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
